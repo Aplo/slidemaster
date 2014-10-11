710b8e0efd286322c0cf9b15def824063e199765
Jack Cook <jack@jackcook.nyc>
2014-10-11 18:07:54 -0400

    Debugging is fun

--- presentation.py
+++ presentation.py
@@ -2,6 +2,8 @@ from bottle import route, run, request
 from bs4 import BeautifulSoup
 import urllib
 import sys
+import os
+from threading import Timer
 
 url = 'https://docs.google.com/presentation/embed?id=1PE-56ZA-Ngm-exuqYLiDR0H19pLXgxK4SzkaKzaJCls&slide='
 current = 1
@@ -9,12 +11,27 @@ total = 0
 
 page = urllib.request.urlopen(url).read()
 bs = BeautifulSoup(page)
-#print(bs.prettify())
+print(bs.find(id=':s'))
 #sys.exit(0)
+
+def check():
+  global current
+  Timer(0.1, check).start()
+  print('check')
+  if os.path.exists('/root/slidemaster/NEXT'):
+    os.remove('/root/slidemaster/NEXT')
+    print('Next slide')
+  elif os.path.exists('/root/slidemaster/PREVIOUS'):
+    os.remove('/root/slidemaster/PREVIOUS')
+    print('Previous slide')
+
+def create():
+  Timer(0.5, create).start()
+  os.system('touch /root/slidemaster/NEXT')
 
 @route('/')
 def hello():
-    return """<!DOCTYPE html>
+  return """<!DOCTYPE html>
 <html>
   <head>
     <title>Test</title>
@@ -48,4 +65,6 @@ def hello():
   </body>
 </html>"""
 
+create()
+check()
 run(host='104.131.83.142', port=80, debug=True)
