ba3d3686da19f629f05dc3bc4a1bb1357f1222b2
Jack Cook <jack@jackcook.nyc>
2014-10-11 17:07:25 -0400

    Made a basic card

--- src/app.js
+++ src/app.js
@@ -6,17 +6,21 @@
 
 var UI = require('ui');
 var Vector2 = require('vector2');
+var ajax = require('ajax');
+
+var cslide = 0;
+var tslides = 1;
 
 var main = new UI.Card({
-  title: 'Pebble.js',
-  icon: 'images/menu_icon.png',
-  subtitle: 'Hello World!',
-  body: 'Press any button.'
+  title: 'Slidemaster',
+  //icon: 'images/menu_icon.png',
+  //subtitle: 'Hello World!',
+  body: 'Slide ' + cslide + ' of ' + tslides
 });
 
 main.show();
 
-main.on('click', 'up', function(e) {
+/*main.on('click', 'up', function(e) {
   var menu = new UI.Menu({
     sections: [{
       items: [{
@@ -34,25 +38,4 @@ main.on('click', 'up', function(e) {
     console.log('The item is titled "' + e.item.title + '"');
   });
   menu.show();
-});
-
-main.on('click', 'select', function(e) {
-  var wind = new UI.Window();
-  var textfield = new UI.Text({
-    position: new Vector2(0, 50),
-    size: new Vector2(144, 30),
-    font: 'gothic-24-bold',
-    text: 'Text Anywhere!',
-    textAlign: 'center'
-  });
-  wind.add(textfield);
-  wind.show();
-});
-
-main.on('click', 'down', function(e) {
-  var card = new UI.Card();
-  card.title('A Card');
-  card.subtitle('Is a Window');
-  card.body('The simplest window type in Pebble.js.');
-  card.show();
-});
+});*/
