a15dfa700ec9022b20cdc56dff0e5e8b28ec750d
Jack Cook <jack@jackcook.nyc>
2014-10-12 01:53:02 -0400

    Added custom presentations

--- src/app.js
+++ src/app.js
@@ -9,16 +9,61 @@ var Vector2 = require('vector2');
 var ajax = require('ajax');
 
 var slide = 1;
-var url = 'http://104.131.83.142';
+var url = 'http://sm.aplo.io';
 
-var main = new UI.Card({
+/*var main = new UI.Card({
   title: 'Slidemaster',
   body: 'Slide ' + slide
 });
 
-main.show();
+main.show();*/
 
-main.on('click', function(e) {
+var main = new UI.Menu();
+
+var titles = [];
+var ids = [];
+ajax(
+  {
+    url: url + '/files',
+    type: 'json'
+  },
+  function(data) {
+    for (var i = 0; i < data.items.length; i++) {
+      if (data.items[i].mimeType == 'application/vnd.google-apps.presentation') {
+        var title = data.items[i].title;
+        titles.push({
+          title: title,
+        });
+        ids.push(data.items[i].id);
+      }
+    }
+    main = new UI.Menu({
+      sections: [{
+        items: titles
+      }]
+    });
+    
+    main.on('select', function(e) {
+      console.log(titles[e.itemIndex].title);
+      var uid = Math.random().toString(36).substr(2, 4);
+      var sid = ids[e.itemIndex];
+      var card = new UI.Card({
+        title: titles[e.itemIndex].title,
+        body: 'id: ' + uid + '\nSlide 1'
+      });
+      ajax(
+        {
+          url: url + '/create?uid=' + uid + '&sid=' + sid
+        }
+      );
+      card.show();
+    });
+    
+    main.show();
+  }
+);
+
+/*main.on('click', function(e) {
   slide += e.button == 'up' ? 1 : -1;
   main.body('Slide ' + slide);
   ajax(
@@ -29,7 +74,7 @@ main.on('click', function(e) {
       main.body('Slide ' + slide);
     }
   );
-});
+});*/
 
 /*main.on('click', 'up', function(e) {
   var menu = new UI.Menu({
